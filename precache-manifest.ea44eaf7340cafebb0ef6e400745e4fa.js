b5f3fb63e3dd4eea551eebbbdf1f27b2dfd56043
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-18 18:04:02 +0000

    Deploy AsAsgard/2019-2-Atom-Frontend-D-Brylkin to github.com/AsAsgard/2019-2-Atom-Frontend-D-Brylkin.git:gh-pages

---
+++ precache-manifest.ea44eaf7340cafebb0ef6e400745e4fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11bf13a4314d5f8e424644db38d1e9bc",
+    "url": "/2019-2-Atom-Frontend-D-Brylkin/index.html"
+  },
+  {
+    "revision": "264cdf0f23fad493a427",
+    "url": "/2019-2-Atom-Frontend-D-Brylkin/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "59bdcefddcc33429c941",
+    "url": "/2019-2-Atom-Frontend-D-Brylkin/static/js/2.2d08f91b.chunk.js"
+  },
+  {
+    "revision": "264cdf0f23fad493a427",
+    "url": "/2019-2-Atom-Frontend-D-Brylkin/static/js/main.43105dbc.chunk.js"
+  },
+  {
+    "revision": "5fbdc9a97fae305e8a7c",
+    "url": "/2019-2-Atom-Frontend-D-Brylkin/static/js/runtime-main.c21db506.js"
+  },
+  {
+    "revision": "a55f1c4358068374afaef45a15ab0eb8",
+    "url": "/2019-2-Atom-Frontend-D-Brylkin/static/media/logo.a55f1c43.svg"
+  }
+]);
